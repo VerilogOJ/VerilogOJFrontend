07f5482394414fc03fc0026180a37afdabbf1bdd
zmyzxb <zmyzxb@hotmail.com>
2022-07-29 11:08:37 +0800

    cfg

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/oj/api": {
-        target: "http://127.0.0.1/oj/api",
+        target: "http://166.111.223.67:40000/oj/api",
         changeOrigin: true,
         pathRewrite: {
           "^/oj/api": "/",
